8e5c190fc7fb6cbfc4fd8ef94599cd529548d7f3
王念 <1194857665@qq.com>
2020-03-27 00:40:39 +0800

    'port'

--- server.js
+++ server.js
@@ -17,6 +17,6 @@ require('./db/connection')
 const bannerRouter = require('./router/bannerRouter')
 app.use('/banner', bannerRouter)
 
-app.listen(3000, () => {
+app.listen(3001, () => {
   console.log('server start')
 })
