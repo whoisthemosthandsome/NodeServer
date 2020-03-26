dd5de6795e797c1d4ac9fc276f62557cabcedb41
王念 <1194857665@qq.com>
2020-03-26 22:10:26 +0800

    'banner2'

--- router/bannerRouter.js
+++ router/bannerRouter.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const router = express.Router()
-const { bannerAdd } = require('../contrls/bannerContrl')
+const { bannerAdd, bannerGet, del } = require('../contrls/bannerContrl')
 const multer = require('multer')
 const upload = multer({})
 const path = require('path')
 const fs = require('fs')
 
+// 添加轮播图
 router.post('/add', upload.single('banner'), (req, res) => {
   let { mimetype, buffer } = req.file
   let url = ''
@@ -21,10 +22,32 @@ router.post('/add', upload.single('banner'), (req, res) => {
   .then(() => {
     res.send({code: 0, msg: '上传成功', url})
   })
-  .catch((err) => {
-    console.log(err)
+  .catch(() => {
     res.send({code: -1, msg: '上传失败'})
   })
 })
 
+// 获取轮播图
+router.post('/get', (req, res) => {
+  bannerGet()
+  .then((list) => {
+    res.send({code: 0, msg: '查询成功', list})
+  })
+  .catch(() => {
+    res.send({code: -1, msg: '查询失败'})
+  })
+})
+
+// 删除轮播图
+router.post('/del', (req, res) => {
+  let { _id } = req.body
+  del(_id)
+  .then(() => {
+    res.send({code: 0, msg: '删除成功'})
+  })
+  .catch((err) => {
+    console.log(err)
+    res.send({code: -1, msg: '删除失败'})
+  })
+})
 module.exports = router
