befa36f0a489089600adbb464480d04a42e69520
Molly-Gu <56762638+Molly-Gu@users.noreply.github.com>
2020-03-29 18:20:12 +0800

    图片

--- db/model/howModel.js
+++ db/model/howModel.js
@@ -2,11 +2,11 @@
 // 评论区模型
 const mongoose =require('mongoose')
 const howSchema = new mongoose.Schema({
-  userName:{ type: String,required:true },//评论对象
+  userName:{ type: String,required:true },//发表评论的用户名
   content: { type: String,required:true },//评论内容
-  url: { type: String,required:false  },//图片路径
+  url: { type: Array,required:false  },//图片路径
   star: { type: String,required:true },//评分
-  staffName:{type: String,required:true },//评论对象
+  staffName:{type: String,required:true },//评论摄影师对象
   // time:{type:Data }
 })
 const howModel = mongoose.model('hows', howSchema)
