fc4c114263d3b559e3132f11c8fc317a24b5e728
王念 <1194857665@qq.com>
2020-03-30 21:51:08 +0800

    'd'

--- router/bookRouter.js
+++ router/bookRouter.js
@@ -22,15 +22,7 @@ router.post('/get',(req,res)=>{
 })
 router.post('/getuser',(req,res)=>{
     let {userName} = req.body
-<<<<<<< HEAD
     bookFind(userName)
-    .then((res)=>{
-        let {list} = res
-        res.send({code:0,msg:'查询成功',list})
-    // console.log({userName})
-=======
->>>>>>> 1e7643eede581b7723e1637a5bb2c0425f9800a2
-    bookFindUser(userName)
     .then((result)=>{
         // let {list} = result
         // console.log(res)
