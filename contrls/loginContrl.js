1e7643eede581b7723e1637a5bb2c0425f9800a2
EastHu <huhejie1997@outlook.com>
2020-03-30 19:47:42 +0800

    hu

--- contrls/loginContrl.js
+++ contrls/loginContrl.js
@@ -27,8 +27,8 @@ const loginModel = require('../db/model/loginModel')
   }
   //修改管理员信息
   const loginUpdata = async (_id,obj) =>{
-    let result = await loginModel.updateOne({_id},{obj})
-    console.log(result)
+    let result = await loginModel.updateOne({_id},obj)
+    // console.log(result)
     return result
   }
 module.exports = {loginAdd,loginUpdata,loginFind,loginDel,loginFindOne,loginup}
