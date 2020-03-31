0958d70e8f5ca8297b42ebaf40d95c4b7e1c9f3c
Molly_Gu <876674577@qq.com>
2020-03-31 22:45:10 +0800

    评论区摄影师查询

--- contrls/howControl.js
+++ contrls/howControl.js
@@ -8,14 +8,14 @@ const picAdd = async ({obj}) =>{
 }
 //评论增加
 let howAdd = async (obj) => {
-  console.log('control',obj)
+  
   let result = await howModel.insertMany(obj)
   return result
 }
 //删除评论
 let howDel = async (_id)=>{//通过传一个评论的id来删除
   let result = await howModel.deleteOne({_id})
-  console.log(result)
+  
   return result
 }
 //用户查看评论
@@ -30,29 +30,38 @@ let allList = async ()=>{
 }
 //分页
 let ByPage = async (page,pageSize)=>{
-  console.log('执行到这里了')
+  
     let  allFood = await howModel.find() 
     // 总数据条数
     let  allCount = allFood.length
     // 每一页的数据
-    let result =await howModel.find().skip((Number(page)-1)*pageSize).limit(Number(pageSize))
+    let result =await howModel.find().sort({_id: -1}).skip((Number(page)-1)*pageSize).limit(Number(pageSize))
    // .populate('staffName', 'phpName -_id')
-    console.log('后端38行',result)
+
     return {result,allCount}
 }
 //通过用户名得到平均分
 let getScore = async (staffName) =>{
   let list = await howModel.find({staffName})
-  console.log(list)
   let allScore=0
   for(var i in list){
     allScore+=Number(list[i].star)
   }
-  console.log('总分',allScore)
   let result=(allScore/list.length)
-  console.log(result)
   return result
 }
+//通过搜索关键字得到摄影师对应信息
+let byKw = async (kw,page,pageSize) =>{
+  let regex = new RegExp(kw)
+  // 满足条件的所有数据
+  let allFood =await  howModel.find({staffName:{$regex:regex}})
+  let allCount= allFood.length
+  // 分页后满足关键字的数据
+  let result= await howModel.find({staffName:{$regex:regex}}).sort({_id: -1})
+  .skip(Number((page-1)*pageSize)).limit(Number(pageSize))
+  return  {result,allCount}
+}
+
 module.exports = {
-  howAdd,howList,howDel,picAdd,allList,ByPage,getScore
+  howAdd,howList,howDel,picAdd,allList,ByPage,getScore,byKw
 }
